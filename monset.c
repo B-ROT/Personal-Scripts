eaea3e99438c0a2fbd2cf9dcae7f1e6bf53df6f9
B-ROT <ferriswheel@tutamail.com>
2025-08-29 16:50:21 +0000

    Add files via upload
    Additional feature: Monitor mode interface will now automatically have a randomized MAC address assigned
    
    Oh, also the output's readability has been increased slightly.

--- monset.c
+++ monset.c
@@ -24,35 +24,40 @@ int main () {
 void SetMonitorMode () {
     printf("\033[0;32mSetting card to monitor mode...\033[0m\n");
 
+    printf("\033[0;32mProcess iwd stopped...\033[0m\n");
     system("sudo systemctl stop iwd");
     system("systemctl status iwd | grep --color=always Active");
-    printf("\033[0;32mProcess iwd stopped...\033[0m\n");
     
-    system("yes | head -n 1 | sudo airmon-ng start 2> /dev/null");
     printf("\033[0;32mMonitor interface online...\033[0m\n");
+    system("yes | head -n 1 | sudo airmon-ng start 2> /dev/null");
     
-    system("ip link show wlan0mon | grep --color=always state");
+    printf("\033[0;32mMAC address of the monitor interface changed...\033[0m\n");
+    system("sudo ip link set wlan0mon down"); 
+    system("sudo macchanger --random --bia wlan0mon");
+    system("sudo ip link set wlan0mon up");
+
     printf("\033[0;32mMonitor mode interface status...\033[0m\n");
+    system("ip link show wlan0mon");
     
-    system("iw dev wlan0mon info | grep --color=always type");
     printf("\033[0;32mMonitor mode set...\033[0m\n");
+    system("iw dev wlan0mon info | grep --color=always type");
 }
 
 void SetManagedMode() {
     printf("\033[0;32mSetting card to managed mode...\033[0m\n");
     
-    system("sudo airmon-ng stop wlan0mon");
     printf("\033[0;32mTaking monitor mode interface offline...\033[0m\n");
+    system("sudo airmon-ng stop wlan0mon");
     
+    printf("\033[0;32mProcess iwd started...\033[0m\n");
     system("sudo systemctl start iwd");
     system("systemctl status iwd | grep --color=always Active");
-    printf("\033[0;32mProcess iwd started...\033[0m\n");
     
     sleep(1);
     
-    system("ip link show wlan0 | grep --color=always state");
     printf("\033[0;32mwlan0 status...\033[0m\n");
+    system("ip link show wlan0");
     
-    system("iw dev wlan0 info | grep --color=always type");
     printf("\033[0;32mwlan0 mode set to managed...\033[0m\n");
+    system("iw dev wlan0 info | grep --color=always type");
 }
